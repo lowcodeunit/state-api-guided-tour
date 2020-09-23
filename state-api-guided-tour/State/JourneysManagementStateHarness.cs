739ab6148ed4cf0cfcf694bb0ea55cb39ea55438
mcgear <michael.gearhardt@gmail.com>
2020-09-23 09:41:52 -0600

    adjusted MF journey content

--- state-api-guided-tour/State/JourneysManagementStateHarness.cs
+++ state-api-guided-tour/State/JourneysManagementStateHarness.cs
@@ -141,8 +141,8 @@ namespace LCU.State.API.NapkinIDE.NapkinIDE.JourneysManagement.State
                     ContentURL = "https://player.vimeo.com/video/403508452",
                     ContentType = JourneyContentTypes.Video,
                     Uses = new List<string>() { "Freeboard", "Data Applications", "Data Flow" },
-                    Description = @"<p>A new first paragraph of text from kilian for MFs.</p>
-                    <p>A second paragraph of text from Kilian for MFs.</p>",
+                    Description = @"<p>An application orchestration environment has been setup that will support you in developing your cloud-native solutions. In this part of the guide, we walk you through deploying and leveraging existing Low Code Units and how to use the Fathym LCU CLI to create and deliver your first micro-frontends.</p>
+                    <p>Read the <a href=""https://fathym-it.com/framework/docs/getting-started/try-it/micro-frontends"">full docs</a> on Fathym Framework micro-frontends now.</p>",
                     Roles = new List<string>(){ "IoT", "Insights" },
                     Active = true,
                     HighlightedOrder = 1,
